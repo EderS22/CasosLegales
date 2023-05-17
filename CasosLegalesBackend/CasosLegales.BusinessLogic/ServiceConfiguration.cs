27fbcb574121f4e098d0a4166c527fde299416d3
AlexProMaster <125411071+AlexProMaster@users.noreply.github.com>
2023-05-17 15:48:51 -0600

    Create API

---
+++ CasosLegalesBackend/CasosLegales.BusinessLogic/ServiceConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CasosLegales.BusinessLogic
+{
+    public static class ServiceConfiguration
+    {
+        public static void DataAcces(this IServiceCollection services, string conection)
+        {
+        
+        }
+
+        public static void BusinessLogic(this IServiceCollection services)
+        {
+           
+        }
+    }
+}
