619241c192dda77ddd5b4316af1e4c11c70dd9a3
EderS22 <54299826+EderS22@users.noreply.github.com>
2023-05-19 02:13:34 -0600

    Plantillas full y limpia

---
+++ angular-app-template-full/src/app/core/services/auth.service.ts
@@ -0,0 +1,103 @@
+import { Injectable } from '@angular/core';
+import { getFirebaseBackend } from '../../authUtils';
+import { User } from '../models/auth.models';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { GlobalComponent } from "../../global-component";
+
+const AUTH_API = GlobalComponent.AUTH_API;
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  
+
+@Injectable({ providedIn: 'root' })
+
+/**
+ * Auth-service Component
+ */
+export class AuthenticationService {
+
+    user!: User;
+    currentUserValue: any;
+
+    private currentUserSubject: BehaviorSubject<User>;
+    // public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')!));
+        // this.currentUser = this.currentUserSubject.asObservable();
+     }
+
+    /**
+     * Performs the register
+     * @param email email
+     * @param password password
+     */
+    register(email: string, first_name: string, password: string) {        
+        // return getFirebaseBackend()!.registerUser(email, password).then((response: any) => {
+        //     const user = response;
+        //     return user;
+        // });
+
+        // Register Api
+        return this.http.post(AUTH_API + 'signup', {
+            email,
+            first_name,
+            password,
+          }, httpOptions);
+    }
+
+     /**
+     * Performs the auth
+     * @param usua_Nombre email of user
+     * @param usua_Clave password of user
+     */
+
+     login(usua_Nombre: string, usua_Clave: string) {
+        // return getFirebaseBackend()!.loginUser(email, password).then((response: any) => {
+        //     const user = response;
+        //     return user;
+        // });
+        return this.http.post(AUTH_API + 'Login/ValidarLogin', {}, {
+            params: {
+               usua_Nombre: usua_Nombre,
+               usua_Clave: usua_Clave
+            }
+        });
+    }
+
+
+    /**
+     * Returns the current user
+     */
+    public currentUser(): any {
+        return getFirebaseBackend()!.getAuthenticatedUser();
+    }
+
+    /**
+     * Logout the user
+     */
+    logout() {
+        // logout the user
+        // return getFirebaseBackend()!.logout();
+        localStorage.removeItem('currentUser');
+        localStorage.removeItem('token');
+        this.currentUserSubject.next(null!);
+    }
+
+    /**
+     * Reset password
+     * @param email email
+     */
+    resetPassword(email: string) {
+        return getFirebaseBackend()!.forgetPassword(email).then((response: any) => {
+            const message = response.data;
+            return message;
+        });
+    }
+
+}
+
